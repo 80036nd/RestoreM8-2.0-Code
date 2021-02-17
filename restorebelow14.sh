1b2d0275b326e9c381260bfeb6c49b9c54b2b31a
80036nd <40246027+80036nd@users.noreply.github.com>
2021-02-17 15:55:38 -0500

    Add files via upload

---
+++ restorebelow14.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+ set -e
+ clear
+ cd ~/Documents/RestoreM8-2.0
+ echo "RestoreM8-2.0 By 80036nd"
+ FILE=restore.ipsw
+ if [ -f "$FILE" ]; then
+ echo "The $FILE file exists. Continuing..."
+ else
+ echo "The $FILE file does not exist on the ~/Documents/RestoreM8-2.0 folder! Cannot continue."
+ echo "Terminal Closing in 10 seconds"
+sleep 10 
+killall Terminal
+ exit
+ fi
+
+ FILE=blob.shsh2
+ if [ -f "$FILE" ]; then
+ echo "The $FILE file exists. Continuing..."
+ else
+ echo "The $FILE file does not exist on the ~/Documents/RestoreM8-2.0 folder! Cannot continue."
+ echo "Terminal Closing in 10 seconds"
+sleep 10 
+killall Terminal
+ exit
+ fi
+
+ FILE=futurerestore180
+ if [ -f "$FILE" ]; then
+ echo "The $FILE file exists. Continuing..."
+ else
+ echo "The $FILE file does not exist on the ~/Documents/RestoreM8-2.0 folder! Cannot continue. Please re-download the code from the app!"
+ exit
+ fi
+ echo "Now transfering the control to FutureRestore! Please monitor the Terminal!"
+ echo "Please wait for FutureRestore to complete."
+ echo "*********************************************************"
+ printf "\n"
+ echo "Welcome to FutureRestore!"
+ chmod 777 futurerestore180
+ ./futurerestore180 -t blob.shsh2 --latest-sep --latest-baseband restore.ipsw
+ clear
+ echo "Your Device Should be Restored"
+ echo "RestoreM8-2.0 By 80036nd"
+ echo "Futurerestore v180 by tihmstar and s0uthwest"
+ echo "Legacy patches by GeoSn0w"
+ echo "Terminal Closing in 10 seconds"
+sleep 10 
+killall Terminal
